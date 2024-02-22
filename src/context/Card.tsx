95da6491d419aca13982014cee58c2a034cb0e23
Rafaelk26 <rafaelk262002@gmail.com>
2024-02-22 19:34:34 -0300

    Update home page, creating component card for page home

---
+++ src/context/Card.tsx
@@ -0,0 +1,12 @@
+export function Card(){
+    return(
+
+        <>
+                {/* Aqui será feito o card dos planetas, galáxias entre outros que ficará no home */}
+                <h1>Teste</h1>
+        
+        </>
+
+
+    )
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,33 @@
+// Components;
+import { Header } from '../../components/Header';
+
+// Image;
+import imgTeste  from '../../assets/images/Planets and Galaxys/Saturno.png';
+
 export function Main(){
     return(
         <>
             <div className="flex justify-center max-h-full">
-            <main className="w-full max-h-screen h-screen flex items-center justify-center">
-                <h1 className="text-white">PÁGINA PRINCIPAL</h1>
+            <main className="w-full min-h-min flex items-center mt-32 flex-col border-2 border-solid border-red-600">
+                <Header className='flex flex-col justify-center items-center bg-transparent'>
+                    <h1 className='text-white font-bold text-5xl'>Lorem</h1>
+                    <nav className='mt-10 text-white'>
+                        <ul className='flex gap-20 text-xl'>
+                            <li>Estrelas</li>
+                            <li>Planetas</li>
+                            <li>Satélites</li>
+                            <li>Galáxias</li>
+                            <li>Outros</li>
+                        </ul>
+                    </nav>
+                </Header>
+                <section className='w-11/12 border-solid border-white border-2'>
+                    <div className='w-1/4 max-w-md border-solid border-blue-600 border-2'>
+                        <div className='w-full p-2 border-2 border-solid border-green-400'>
+                            <img src={imgTeste} alt={'teste'} />
+                        </div>
+                    </div>
+                </section>
             </main>
             </div>
         </>
